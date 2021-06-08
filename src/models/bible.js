6c1f10d33bb911eeeac8af77af0a544db7d397a1
Paceaux <frank.m.taylor08@gmail.com>
2021-06-08 16:06:32 -0500

    creates an abstract class

--- src/models/bible.js
+++ src/models/bible.js
@@ -1,31 +1,13 @@
-const BibleService = require('../bibleService');
-class  Bible {
-    id = '';
-    data = {};
-    bibleService = new BibleService();
+const BibleEntity = require('./entity');
+class  Bible extends BibleEntity {
 
     /**
      * @param  {object|string} data full data object returned from api, or the id of a bible
      * @param  {BibleService|string} bibleService the BibleService, or the apikey that could be used to instantiate a service
      */
     constructor(data, bibleService ) {
+        super(data, bibleService);
 
-        if (typeof data === 'string') {
-            this.id = data;
-        }
-
-        if (typeof data === 'object') {
-            if ('id' in data) this.id = data.id;
-            this.data = data;
-        }
-
-        if (typeof bibleService === 'string') {
-            this.bibleService = new BibleService(bibleService);
-        }
-
-        if (bibleService instanceof BibleService) {
-            this.bibleService = bibleService;
-        }
     }
 
     async refreshData() {
---
+++ src/models/entity.js
@@ -0,0 +1,34 @@
+const BibleService = require('../bibleService');
+
+class BibleEntity {
+
+    id = '';
+    data = {};
+    bibleService = new BibleService();
+
+    /**
+     * @param  {object|string} data full data object returned from api, or the id of a bible
+     * @param  {BibleService|string} bibleService the BibleService, or the apikey that could be used to instantiate a service
+     */
+    constructor(data, bibleService ) {
+
+        if (typeof data === 'string') {
+            this.id = data;
+        }
+
+        if (typeof data === 'object') {
+            if ('id' in data) this.id = data.id;
+            this.data = data;
+        }
+
+        if (typeof bibleService === 'string') {
+            this.bibleService = new BibleService(bibleService);
+        }
+
+        if (bibleService instanceof BibleService) {
+            this.bibleService = bibleService;
+        }
+    }
+}
+
+module.exports = BibleEntity;
