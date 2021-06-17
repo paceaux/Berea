43dedc748d736569735236d92f579a2d9bb64312
Paceaux <frank.m.taylor08@gmail.com>
2021-06-17 13:44:11 -0500

    Adds documentation and test for book model

--- src/models/book.js
+++ src/models/book.js
@@ -1,27 +1,82 @@
 const BibleEntity = require('./bible-entity');
+const Chapter = require('./chapter');
 
+/**
+ * @type {import('../bibleService').Book}
+ */
 class Book extends BibleEntity {
+
+    chapters = [];
+
+    /**
+     * @param  {Object} data
+     * @param  {import('../bibleService').Bible} bible
+     */
     constructor(data, bible) {
         super(data, bible);
-
     }
+    
+    /** Name of the book
+     * @type {string}
+     */
     get name() {
         return this.data.name;
     }
 
+    /**
+     * Full name (or short description)
+     * @type {string}
+     */
     get longName() {
         return this.data.nameLong;
     }
 
+    /**
+     * Abbreviation used by the API
+     * @type {string}
+     */
     get abbreviation() {
         return this.data.abbreviation;
     }
 
+
+    /**
+     * Populates data if constructed with a string, or retrieves data not present at instantiation
+     * This will load the chapters property with chapters
+     * 
+     * @returns {Promise<void>}
+     */
     async refreshData() {
-        const data = await this.bibleService.getBook(this.id);
+        const data = await this.bibleService.getBook(this.bible.id, this.id);
+        const chapters = await this.getChapters();
         this.data = data;
+        this.chapters = chapters;
     }
 
+
+    /**
+     * Retrieves all chapters for this book
+     * @param  {import('../bibleService').ChapterRequestParam} params={}
+     *
+     * @returns {Array<Chapter>}
+     */
+    async getChapters(params = {}) {
+        const request = {...params, bookId: this.id, id: this.bible.id };
+        let result = null;
+
+        try {
+            const chapters = await this.bibleService.getChaptersFromBook(request)
+            result = chapters.map((chapter) => {
+                return new Chapter(chapter, this.bible);
+            });
+        } catch (getError) {
+            console.log(getError);
+            result = getError;
+        }
+
+        return result;
+
+    }
 }
 
 module.exports = Book;
---
+++ test/model/book.test.js
@@ -0,0 +1,74 @@
+const BibleService = require('../../src/bibleService');
+const Bible = require('../../src/models/bible');
+const Book = require('../../src/models/book');
+const Chapter = require('../../src/models/chapter');
+
+
+const service = new BibleService('5ae573a324440896fabd2942943728a5', 1);
+const bible = new Bible('06125adad2d5898a-01', service);
+const bookMock = {
+        "id": "GEN",
+        "bibleId": "06125adad2d5898a-01",
+        "abbreviation": "Gen.",
+        "name": "Genesis",
+        "nameLong": "The First Book of Moses, Commonly Called Genesis"
+};
+
+
+describe('Model: Book', () => {
+    describe('constructor', () => {
+        describe('default props', () => {
+            const book = new Book({},bible);
+            it('has an id', () => {
+                expect(book).toHaveProperty('id', '');
+            }); 
+            it('has empty data', () => {
+                expect(book).toHaveProperty('data');
+                expect(book.data).toMatchObject({});
+            }); 
+            it('has a service', () => {
+                expect(book).toHaveProperty('bibleService');
+                expect(book.bibleService).toBeInstanceOf(BibleService);
+            });
+        });
+        describe('book Props', () => {
+            const book = new Book(bookMock,bible);
+            it('has an name', () => {
+                expect(book).toHaveProperty('name', bookMock.name);
+            }); 
+            it('has a longname', () => {
+                expect(book).toHaveProperty('longName', bookMock.nameLong);
+            }); 
+            it('has an abbreviation', () => {
+                expect(book).toHaveProperty('abbreviation', bookMock.abbreviation);
+            }); 
+            it('has an id', () => {
+                expect(book).toHaveProperty('id', bookMock.id);
+            }); 
+        });
+    });
+    describe('refreshData', () => {
+        it(' will populate data', async () => {
+            const book = new Book('GEN', bible);
+
+            await book.refreshData();
+            expect(book.data).toHaveProperty('bibleId');
+        });
+    });
+    describe('getChapters', () => {
+        it(' will get an array', async () => {
+            const book = new Book(bookMock, bible);
+
+            const chapters = await book.getChapters();
+            expect(chapters).toBeInstanceOf(Array);
+            expect(chapters.length).toBeGreaterThan(10);
+        });
+        it(' will get an array of Chapters', async () => {
+            const book = new Book(bookMock, bible);
+
+            const chapters = await book.getChapters();
+            expect(chapters[0]).toBeInstanceOf(Chapter);
+
+        });
+    });
+});
