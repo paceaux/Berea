cb6a3cd6e97c63158df21345c7dc0d7b80f48a94
Paceaux <frank.m.taylor08@gmail.com>
2021-06-21 15:54:54 -0500

    adds verse model

--- src/models/verse.model.js
+++ src/models/verse.model.js
@@ -1,15 +1,53 @@
 const BibleEntity = require('./bible-entity.model');
 const Chapter = require('./chapter.model');
 
-
+/**
+ * @type {import('../bibleService').Verse}
+ */
 class Verse extends BibleEntity {
+    /**
+     * @param  {Object} data
+     * @param  {import('../bibleService').Bible} bible
+     */
     constructor(data, bible) {
         super(data, bible);
 
     }
 
+    /** Name of book and chapter, or undefined if data not refreshed
+     * @type {string|undefined} 
+     */
+    get name() {
+        return this.data.reference;
+    }
+
+    /** id for the book which contains this chapter
+     * @type {string}
+     */
+    get bookId() {
+        const [book] = this.id.split('.');
+        return `${book}`;
+    }
+
+    /** id for the chapter which contains this chapter
+     * @type {string}
+     */
+    get chapterId() {
+        const [book, chapterNum] = this.id.split('.');
+        return `${book}.${chapterNum}`;
+    }
+
+    /** chapter number. Intros will return 0
+     * @type {number} 
+     */
+    get number() {
+        const [book, chapterNum, verseNum] = this.id.split('.');
+        const number = parseInt(verseNum, 10);
+        return isNaN(number) ? 0 : number;
+    }
+
     get chapter() {
-        const { chapterId } = this.data;
+        const { chapterId } = this;
         let chapter = null;
 
         if (chapterId) {
@@ -18,21 +56,93 @@ class Verse extends BibleEntity {
         return chapter;
     }
 
-    get verseCount() {
-        const { verseCount } = this.data;
-        
-        return verseCount ? verseCount : 0;
+    /** Trimmed raw content from API if data has been refreshed or loaded
+     * @type {string}
+     */
+    get content() {
+        const strContent = this.data.content;
+        const { number } = this;
+        let content = '';
+
+        if (strContent) {
+            content = strContent
+                .replace(`[${number}]`, '')
+                .trim();
+        }
+
+        return content;
+    }
+    /** The previous verse in the chapter, or null if data is not refreshed/loaded
+     * @type {Chapter|null}
+     */
+    get previousVerse() {
+        let verse = null;
+        const { data : { previous }} = this;
+
+        if (previous && previous.id) {
+            verse = new Verse(previous.id, this.bible);
+        }
+        return verse;
     }
 
-    get content() {
-        const { content } = this.data;
+    /** The next verse in the chapter, or null if data is not refreshed/loaded
+     * @type {Chapter|null}
+     */
+    get nextVerse() {
+        let verse = null;
+        const { data : { next }} = this;
+        
+        if (next && next.id) {
+            verse = new Verse(next.id, this.bible);
+        }
+        return verse;
+    }
 
-        return content || '';
+    /** gets previous verse in the book
+     * @returns {Verse|null}
+     */
+    async getPrevious() {
+        let verse = this.previousVerse;
+        if (!verse) return null;
+        
+        try {
+            await verse.refreshData();
+        } catch (getVerseError) {
+            console.error(getChapterError);
+        }
+
+        return verse;
+    }
+
+    /** gets next verse in the book
+     * @returns {Verse|null}
+     */
+    async getNext() {
+        let verse = this.nextVerse;
+        if (!verse) return null;
+
+        try {
+            await verse.refreshData();
+        } catch (getVerseError) {
+            console.error(getVerseError);
+        }
+
+        return verse;
     }
     
-
+    /**
+     * Populates data if constructed with a string, or retrieves data not present at instantiation
+     * This will load the content, verses, and previous/nextChapters properties
+     * 
+     * @returns {Promise<void>}
+     */
     async refreshData() {
-        const data = await this.bibleService.getVerse(this.id);
+        const data = await this.bibleService.getVerse({
+            contentType: 'text',
+            id: this.bible.id,
+            verseId: this.id,
+        });
+
         this.data = data;
     }
 
---
+++ test/model/verse.model.test.js
@@ -0,0 +1,125 @@
+const BibleService = require('../../src/bibleService');
+const Bible = require('../../src/models/bible.model');
+const Chapter = require('../../src/models/chapter.model');
+const Verse = require('../../src/models/verse.model');
+
+
+const service = new BibleService('5ae573a324440896fabd2942943728a5', 1);
+const bible = new Bible('06125adad2d5898a-01', service);
+const verseMock = {
+        "id": "GEN.1.1",
+        "bookId": "GEN",
+        "chapterId": "GEN.1",
+        "reference": "Genesis 1:1",
+        "content": "     [1] In the beginning God created the heavens and the earth. \n",
+};
+
+
+describe('Model: Verse', () => {
+    describe('constructor', () => {
+        describe('default props', () => {
+            const verse = new Verse({},bible);
+            it('has an id', () => {
+                expect(verse).toHaveProperty('id', '');
+            }); 
+            it('has empty data', () => {
+                expect(verse).toHaveProperty('data');
+                expect(verse.data).toMatchObject({});
+            }); 
+            it('has a service', () => {
+                expect(verse).toHaveProperty('bibleService');
+                expect(verse.bibleService).toBeInstanceOf(BibleService);
+            });
+        });
+        describe('verse Props', () => {
+            const verse = new Verse(verseMock,bible);
+            it('has an id', () => {
+                expect(verse).toHaveProperty('id', verseMock.id);
+            }); 
+            it('has a number', () => {
+                expect(verse).toHaveProperty('number', 1);
+            }); 
+            it('has a chapterId', () => {
+                expect(verse).toHaveProperty('chapterId', verseMock.chapterId);
+            }); 
+            it('has a bookId', () => {
+                expect(verse).toHaveProperty('bookId', verseMock.bookId);
+            }); 
+            it('has a name', () => {
+                expect(verse).toHaveProperty('name', verseMock.reference);
+            }); 
+            it('has a content', () => {
+                expect(verse).toHaveProperty('content');
+            }); 
+            it('verse content doesn\'t have the verse number in it', async () => {
+                expect(verse.content).toEqual(expect.not.stringContaining('[1]'))
+            });
+        });
+    });
+    describe('refreshData', () => {
+        it(' will populate data', async () => {
+            const verse = new Verse('GEN.1.1', bible);
+
+            await verse.refreshData();
+            expect(verse.data).toHaveProperty('bookId');
+            expect(verse.data).toHaveProperty('copyright');
+            expect(verse.data).toHaveProperty('previous');
+            expect(verse.data).toHaveProperty('next');
+
+        });
+        it('dynamic props will have info', async () => {
+            const verse = new Verse('GEN.1.1', bible);
+
+            await verse.refreshData();
+            expect(verse.content.length).toBeGreaterThan(0);
+            expect(verse.data.copyright.length).toBeGreaterThan(0);
+
+        });
+    });
+    describe('prev and next', () => {
+        it('has empty prev and next at start', () => {
+            const verse = new Verse('GEN.1.1', bible);
+            expect(verse.previousVerse).toBe(null);
+            expect(verse.nextVerse).toBe(null);
+        });
+        it('When refreshed, prev/next are of type Verse', async () => {
+            const verse = new Verse('GEN.1.1', bible);
+
+            await verse.refreshData();
+
+            expect(verse.previousVerse).toBeInstanceOf(Verse);
+            expect(verse.nextVerse).toBeInstanceOf(Verse);
+        });
+        it('gets full data with getNext', async () => {
+            const verse = new Verse('GEN.1.1', bible);
+
+            await verse.refreshData();
+
+            const next = await verse.getNext();
+
+            expect(next.content).toBeTruthy();
+        });
+        it('gets full data with getPrevious', async () => {
+            const verse = new Verse('GEN.1.2', bible);
+
+            await verse.refreshData();
+
+            const previous = await verse.getPrevious();
+
+            expect(previous.content).toBeTruthy();
+        });
+    });
+    describe('intro edge cases', () => {
+        it('Intro verse has a number of 0', async ()=> {
+            const verse = new Verse('GEN.intro.0', bible);
+            await verse.refreshData();
+            
+            expect(verse.number).toEqual(0);
+        });
+        it('previous verse does not exist', async ()=> {
+            const chapter = new Verse('GEN.intro.0', bible);
+            await chapter.refreshData();
+            expect(chapter.previousVerse).toBeNull();
+        });
+    });
+});
