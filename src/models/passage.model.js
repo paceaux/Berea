bcb3939df381fc5a93e596906bca3829cd406dfb
Paceaux <frank.m.taylor08@gmail.com>
2021-06-22 14:42:45 -0500

    adds passage tests

--- src/models/passage.model.js
+++ src/models/passage.model.js
@@ -1,33 +1,148 @@
 const BibleEntity = require('./bible-entity.model');
 const Chapter = require('./chapter.model');
 
+/**
+ * @type {import('../bibleService').Passage}
+ */
 class Passage extends BibleEntity {
+  /** Name of book and chapter, or undefined if data not refreshed
+     * @type {string|undefined}
+     */
+  get name() {
+    return this.data.reference;
+  }
+
+  /** Id of the first verse in the passage
+     * @type {string}
+     */
+  get firstVerseId() {
+    const [start] = this.id.split('-');
+
+    return start;
+  }
+
+  /** Id of the last verse in the passage
+     * @type {string}
+     */
+  get lastVerseId() {
+    const [, last] = this.id.split('-');
+
+    return last;
+  }
+
+  /** id for the book which contains this passage
+     * @type {string}
+     */
+  get bookId() {
+    const [book] = this.id.split('.');
+    return `${book}`;
+  }
+
+  /** ids for the chapters the passage spans
+     * @type {Array<string>}
+     */
+  get chapterIds() {
+    const { firstVerseId, lastVerseId, bookId } = this;
+    const firstChapterNum = parseInt(firstVerseId.split('.')[1], 10);
+    const lastChapterNum = parseInt(lastVerseId.split('.')[1], 10);
+    const chapterNumbers = [firstChapterNum, lastChapterNum];
+
+    while (chapterNumbers[chapterNumbers.length - 1] - 1 !== chapterNumbers[chapterNumbers.length - 2]) {
+      chapterNumbers.splice(chapterNumbers.length - 1, 0, chapterNumbers[chapterNumbers.length - 2] + 1);
+    }
+
+    return chapterNumbers
+      .map((chapterNumber) => `${bookId}.${chapterNumber}`);
+  }
+
+  /** Chapters the passage spans
+   * @type {Array<string}
+   */
   get chapters() {
-    const { chapterIds } = this.data;
+    const { chapterIds, data } = this;
     const chapters = [];
+    const chapterRegex = new RegExp(/\s(?=\[1\])/);
+    const verseRegex = new RegExp(/(?:\[[0-9]+\]\s)/);
 
     if (chapterIds && chapterIds.length) {
+      const { content } = data;
+      const chapterContents = [];
+
+      if (content) {
+        const chaptersSplitByRegex = content.split(chapterRegex);
+        const chapterArray = chaptersSplitByRegex.map((chapterText) => chapterText.trim().replace('\n', ''));
+        chapterContents.push(...chapterArray.filter((el) => el));
+      }
+
       const chapterObjects = chapterIds
-        .map((chapterId) => new Chapter(chapterId, this.bibleService));
+        .map((chapterId, index) => {
+          const chapter = new Chapter(chapterId, this.bible);
+          const chapterContent = content && chapterContents[index];
+
+          if (chapterContent) {
+            chapter.data.content = chapterContents;
+            const verseSplit = chapterContent.split(verseRegex);
+            const verseArray = verseSplit.map((verse) => verse.trim().replace('\n', ''));
+            const verses = verseArray.filter((el) => el);
+            chapter.data.verseCount = verses.length;
+          }
+          return chapter;
+        });
       chapters.push(...chapterObjects);
     }
     return chapters;
   }
 
+  /** number of verses in the chapter or 0 if data has not been refreshed or loaded
+     * @type {number}
+     */
   get verseCount() {
-    const { verseCount } = this.data;
-
-    return verseCount || 0;
+    return this.data.verseCount || 0;
   }
 
+  /** Array of verses if data has been refreshed or loaded
+     * @type {array<string>}
+     */
+  get verseList() {
+    const verses = [];
+
+    if (typeof this.content === 'string' && this.verseCount > 0) {
+      const splitRegex = new RegExp(/(?:\[[0-9]+\]\s)/);
+      const verseSplit = this.content.split(splitRegex);
+      const verseArray = verseSplit.map((verse) => verse.trim().replace('\n', ''));
+      verses.push(...verseArray.filter((el) => el));
+    }
+    return verses;
+  }
+
+  /** Trimmed raw content from API if data has been refreshed or loaded
+     * @type {string}
+     */
   get content() {
-    const { content } = this.data;
+    const strContent = this.data.content;
+    let content = '';
 
-    return content || '';
+    if (strContent) {
+      content = strContent.trim();
+    }
+
+    return content;
   }
 
+  /**
+     * Populates data if constructed with a string, or retrieves data not present at instantiation
+     * This will load the content, verses, and previous/nextChapters properties
+     *
+     * @returns {Promise<void>}
+     */
   async refreshData() {
-    const data = await this.bibleService.getPassage(this.id);
+    const data = await this.bibleService.getPassage(
+      {
+        contentType: 'text',
+        id: this.bible.id,
+        passageId: this.id,
+      },
+    );
     this.data = data;
   }
 }
---
+++ test/model/passage.model.test.js
@@ -0,0 +1,96 @@
+const BibleService = require('../../src/bibleService');
+const Bible = require('../../src/models/bible.model');
+const Chapter = require('../../src/models/chapter.model');
+const Passage = require('../../src/models/passage.model');
+
+const service = new BibleService('5ae573a324440896fabd2942943728a5', 1);
+const bible = new Bible('06125adad2d5898a-01', service);
+const passageMock = {
+  id: 'GEN.1.1-GEN.4.20',
+  orgId: 'GEN.1.1-GEN.4.20',
+  bibleId: '06125adad2d5898a-01',
+  bookId: 'GEN',
+};
+
+describe('Model: Passage', () => {
+  describe('constructor', () => {
+    describe('default props', () => {
+      const passage = new Passage({}, bible);
+      it('has an id', () => {
+        expect(passage).toHaveProperty('id', '');
+      });
+      it('has empty data', () => {
+        expect(passage).toHaveProperty('data');
+        expect(passage.data).toMatchObject({});
+      });
+      it('has a service', () => {
+        expect(passage).toHaveProperty('bibleService');
+        expect(passage.bibleService).toBeInstanceOf(BibleService);
+      });
+    });
+    describe('passage Props', () => {
+      const passage = new Passage(passageMock, bible);
+      it('has an id', () => {
+        expect(passage).toHaveProperty('id', passageMock.id);
+      });
+      it('has a name', () => {
+        expect(passage).toHaveProperty('name', passageMock.reference);
+      });
+      it('has a startVerseid', () => {
+        expect(passage).toHaveProperty('firstVerseId', 'GEN.1.1');
+      });
+      it('has an  endVerseId', () => {
+        expect(passage).toHaveProperty('lastVerseId', 'GEN.4.20');
+      });
+      it('has a bookId', () => {
+        expect(passage).toHaveProperty('bookId', passageMock.bookId);
+      });
+      it('has chapterIds', () => {
+        expect(passage).toHaveProperty('chapterIds');
+        expect(passage.chapterIds).toEqual(expect.arrayContaining(['GEN.1', 'GEN.2', 'GEN.3', 'GEN.4']))
+      });
+      it('has verseCount', () => {
+        expect(passage).toHaveProperty('verseCount');
+      });
+      it('has a content', () => {
+        expect(passage).toHaveProperty('content');
+      });
+    });
+  });
+  describe('refreshData', () => {
+    it(' will populate data', async () => {
+      const passage = new Passage('GEN.1.1-GEN.4.20', bible);
+
+      await passage.refreshData();
+      expect(passage.data).toHaveProperty('bookId');
+      expect(passage.data).toHaveProperty('copyright');
+      expect(passage.data).toHaveProperty('verseCount', 100);
+    });
+    it('dynamic props will have info', async () => {
+      const passage = new Passage('GEN.1.1-GEN.4.20', bible);
+
+      await passage.refreshData();
+      expect(passage.content.length).toBeGreaterThan(0);
+      expect(passage.data.copyright.length).toBeGreaterThan(0);
+    });
+    it('will have a property containing chapter objects', async () => {
+      const passage = new Passage('GEN.1.1-GEN.4.20', bible);
+
+      await passage.refreshData();
+      expect(passage.chapters).toBeInstanceOf(Array);
+      expect(passage.chapters.length).toBeGreaterThan(0);
+      expect(passage.chapters[0]).toBeInstanceOf(Chapter);
+    });
+    it('will have verse content in its chapter objects', async () => {
+      const passage = new Passage('GEN.1.1-GEN.4.20', bible);
+
+      await passage.refreshData();
+
+      const [firstChapter] = passage.chapters;
+
+      expect(firstChapter).toHaveProperty('data');
+      expect(firstChapter.data).toHaveProperty('content');
+      expect(firstChapter.data).toHaveProperty('verseCount');
+    });
+  });
+});
